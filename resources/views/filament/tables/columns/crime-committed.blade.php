ef323ec1e87af3d34d16215e01b17cf689ad5d23
johnreynaceda <johnreynaceda@sksu.edu.ph>
2024-02-27 16:50:41 +0800

    Update crime-committed.blade.php

--- resources/views/filament/tables/columns/crime-committed.blade.php
+++ resources/views/filament/tables/columns/crime-committed.blade.php
@@ -1,1 +1,6 @@
-a
+@if ($getRecord()->pdlcases->count() > 1)
+    {{ $getRecord()->pdlcases->first()->crime->name }} and <span
+        class="text-green-600 hover:text-blue-700 cursor-pointer">
+        <button wire:click="viewCommitedCrime({{ $getRecord()->id }})">more..</button>
+    </span>
+@endif
