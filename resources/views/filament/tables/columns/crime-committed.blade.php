1df0ea79de5bf8c19c67cdeb64f235bfd34d044d
johnreynaceda <johnreynaceda@sksu.edu.ph>
2024-02-28 23:00:01 +0800

    Update crime-committed.blade.php

--- resources/views/filament/tables/columns/crime-committed.blade.php
+++ resources/views/filament/tables/columns/crime-committed.blade.php
@@ -3,4 +3,6 @@
         class="text-green-600 hover:text-blue-700 ml-3 text-sm cursor-pointer">
         <button wire:click="viewCommitedCrime({{ $getRecord()->id }})"> more..</button>
     </span>
+@else
+    {{ $getRecord()->pdlcases->first()->crime->name }}
 @endif
